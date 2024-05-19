b1fa5fc7f3e92f060038459002311a7d076fa7fe
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-19 18:47:22 -0400

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //api key i created on the weathermap site
 var apiKey="e8f779b30d9bec703742332c39c699e7"
-
+var dashboardEl=document.getElementById("dashboard")
 //function to pull weathermap api
 function currentWeather(cityName){
     var url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
@@ -11,10 +11,15 @@ function currentWeather(cityName){
     })
     .then(function(data){
         console.log(data)
+
+        dashboardEl.innerHTML=` <h3> ${data.name} ${dayjs.unix(data.dt).format("MM/DD/YYYY")} <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""></h3>
+        <p>Temp:${data.main.temp}</p>
+        <p>Wind Speed:${data.wind.speed}</p>
+        <p>Humidity:${data.main.humidity}</p>`
     })
 }
-function fivedayForecast(){
-    var url=`api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+function fivedayForecast(cityName){
+    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
 
     fetch(url)
     .then(function(response){
@@ -22,9 +27,16 @@ function fivedayForecast(){
     })
     .then(function(data){
         console.log(data)
+
+        for(var i=3; i<data.list.length; i=i+8){
+            var fiveDayArr=data.list
+            console.log(fiveDayArr[i])
+        }
     })
 }
 
 
 //calls the currentWeather function
 currentWeather('Chicago')
+//calls fivedayForecast
+fivedayForecast('Chicago')
