cfff374e4c3b9759b745594277fa894040bb1639
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-19 16:53:07 -0400

    changed units to imperlia for weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
+//api key i created on the weathermap site
 var apiKey="e8f779b30d9bec703742332c39c699e7"
 
+//function to pull weathermap api
+function currentWeather(cityName){
+    var url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
 
-function currentWeather(){
-    
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
 }
+//calls the currentWeather function
+currentWeather('Chicago')
